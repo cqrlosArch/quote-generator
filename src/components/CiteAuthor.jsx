2feb602999c153e5a47041e2ad018d70435af481
cqrlosArch <diaz.flores.c@gmail.com>
2020-10-30 20:37:51 +0100

    author

--- src/components/CiteAuthor.jsx
+++ src/components/CiteAuthor.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 const CiteAuthorStyled = styled.footer`
-  font-size: 24px;
-  line-height: 28px;
   color: #4f4f4f;
   width: 100%;
   padding: 1rem;
@@ -11,16 +9,18 @@ const CiteAuthorStyled = styled.footer`
   justify-content: space-between;
   align-items: center;
   cursor: pointer;
-  @media screen and (max-width: 850px){
-      
-      font-size: 20px;
-    }
+  @media screen and (max-width: 850px) {
+    font-size: 20px;
+  }
 
   &:hover {
     background-color: #333333;
     color: #fff;
   }
-
+  .author {
+    font-size: 24px;
+    line-height: 28px;
+  }
   .genre {
     font-size: 14px;
     line-height: 16px;
@@ -29,15 +29,14 @@ const CiteAuthorStyled = styled.footer`
   .icon {
     color: #fff;
     font-size: 40px;
-   
   }
 `;
 const CiteAuthor = ({ quote, getRandomQuotesAuthor }) => {
-  const {  quoteAuthor, quoteGenre } = quote;
+  const { quoteAuthor, quoteGenre } = quote;
   return (
-    <CiteAuthorStyled onClick={()=>getRandomQuotesAuthor(quoteAuthor)}>
+    <CiteAuthorStyled onClick={() => getRandomQuotesAuthor(quoteAuthor)}>
       <div className="container">
-        {quoteAuthor && <p>{quoteAuthor}</p>}
+        <h2 className="author">{quoteAuthor}</h2>
         <p className="genre">{quoteGenre}</p>
       </div>
       <span className="material-icons icon md-48">arrow_right_alt</span>
